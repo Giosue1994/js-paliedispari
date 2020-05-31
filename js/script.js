4296d9b15c5b9496cd43c8effe6291a0a71173e3
Giosue1994 <giosuelentini1994@outlook.com>
2020-05-31 18:47:49 +0200

    pari o dispari html

--- js/script.js
+++ js/script.js
@@ -38,51 +38,11 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-//variabili;
-var scegliPOD;
-var numeroUtente;
-var numeroComputer;
-var message;
-var somma;
-
-// l'utente sceglie pari o dispari
-scegliPOD = prompt('Scegli pari o dispari');
-while ((!isNaN(scegliPOD)) || (scegliPOD != ('pari') && scegliPOD != ('dispari'))) {
-  scegliPOD = prompt('Scegli pari o dispari');
-}
-message = 'Hai scelto ' + scegliPOD;
-console.log(message);
-
-// l'utente sceglie un numero da 1 a 5
-numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
-while ((isNaN(numeroUtente)) || ((numeroUtente > 5) || (numeroUtente === 0))) {
-  numeroUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
-}
-message = 'Hai scelto ' + numeroUtente;
-console.log(message);
-
-// il computer genera un numero casuale da 1 a 5
-numeroComputer = nRandom1To5(1, 5);
-message = 'Il computer ha scelto ' + numeroComputer;
-console.log(message);
-
-// somma dei numeri scelti
-somma = numeroUtente + numeroComputer;
-message = 'la somma dei numeri è ' + somma;
-console.log(message);
-
-// vince chi indovina se la somma dei numeri è pari o dispari
-if (isPOD(somma) === scegliPOD) {
-  message = 'hai vinto';
-}else {
-  message = 'hai perso';
-}
-console.log(message);
-
-// FUNZIONE NUMERO RANDOM DA 1 A 5
+// FUNZIONE NUMERO RANDOM DA num A num
 function nRandom1To5(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
+
 // FUNZIONE PARI O DISPARI
 function isPOD(pod) {
   if (pod % 2 === 0) {
@@ -93,3 +53,71 @@ function isPOD(pod) {
 
   return pariODispari;
 }
+
+// VAR BOTTONI
+var avvia = document.getElementById('avvia');
+var annulla = document.getElementById('annulla');
+
+// VARIABILI
+var scegliPOD = document.getElementById('scegli-pod');
+var numeroUtente = document.getElementById('numero-utente');
+var resultPOD = document.getElementById('result-pod');
+var resultNumUtente = document.getElementById('result-num-utente');
+var resultNumComputer = document.getElementById('result-num-computer');
+var resultSomma = document.getElementById('result-somma');
+var winLose = document.getElementById('win-lose');
+
+var numeroComputer;
+var somma;
+
+avvia.addEventListener('click',
+  function() {
+
+    // l'utente sceglie pari o dispari
+    if ((!isNaN(scegliPOD.value)) || (scegliPOD.value != ('pari') && scegliPOD.value != ('dispari'))) {
+      resultPOD.innerHTML = 'Campo errato';
+    }else {
+      resultPOD.innerHTML = 'Hai scelto: ' + scegliPOD.value;
+    }
+
+
+    // l'utente sceglie un numero da 1 a 5
+    if ((isNaN(numeroUtente.value)) || ((numeroUtente.value > 5) || (numeroUtente.value == 0))) {
+      resultNumUtente.innerHTML = 'Campo errato';
+    }else {
+      resultNumUtente.innerHTML = 'Hai scelto: ' + numeroUtente.value;
+    }
+
+    // il computer genera un numero casuale da 1 a 5
+    numeroComputer = nRandom1To5(1, 5);
+    resultNumComputer.innerHTML ='Il computer ha scelto: ' +  numeroComputer;
+
+    // somma dei numeri scelti
+    if (somma = parseInt(numeroUtente.value) + numeroComputer) {
+      resultSomma.innerHTML = 'La somma dei numeri è: ' + somma;
+    }else {
+      resultSomma.innerHTML = 'Completare i campi per avere un risultato.';
+    }
+
+
+    // vince chi indovina se la somma dei numeri è pari o dispari
+    if (isPOD(somma) === scegliPOD.value) {
+      winLose.innerHTML = 'HAI VINTO!';
+    }else {
+      winLose.innerHTML = 'HAI PERSO!';
+    }
+  }
+);
+
+annulla.addEventListener('click',
+function() {
+  scegliPOD.value = '';
+  numeroUtente.value = '';
+
+  resultPOD.innerHTML = '';
+  resultNumUtente.innerHTML = '';
+  resultNumComputer.innerHTML = '';
+  resultSomma.innerHTML = '';
+  winLose.innerHTML = '';
+}
+);
